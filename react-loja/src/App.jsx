96582746bd72dfe4b638978768e5aab71442fbea
CaioUno <66970652+CaioUno@users.noreply.github.com>
2021-02-10 22:06:16 -0300

    Installing libraries
    Installed bootstrap, react-bootstrap, jquery and popperjs

---
+++ react-loja/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <span>TESTE DE REACT</span>
+      </div>
+    )
+  }
+}
